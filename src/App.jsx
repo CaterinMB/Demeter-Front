6bae2e5ed21a8c13aaa6fda5beb9c37d283f5c3e
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-18 11:13:47 -0500

    App

--- src/App.jsx
+++ src/App.jsx
@@ -54,57 +54,57 @@ function App() {
       <Role>
         <User>
           <Module>
-            <DashboardProvider>
+            {/* <DashboardProvider> */}
               <CategorySupplies>
                 <CategoryProducts>
                   <Product>
                     <Supplies>
                       <Losses>
-                        <ShoppingProvider>
+                        {/* <ShoppingProvider>
                           <SaleProvider>
                             <ProductCategoriesProvider>
                               <ProductProvider>
-                                <Supplier>
+                                <Supplier> */}
                                   <Header />
                                   <Navbar />
                                   {/* <NavbarHeader /> */}
                                   <ErrorBoundary>
                                     <Routes>
                                       <Route path='/' element={<Login />} />
-                                      <Route path='/resetPassword' element={<ResetPassword />} />
-                                      <Route path='/newPassword/:idUser' element={<NewPassword />} />
+                                      {/* <Route path='/resetPassword' element={<ResetPassword />} /> */}
+                                      {/* <Route path='/newPassword/:idUser' element={<NewPassword />} /> */}
                                       <Route element={<ProtectedRoute />}>
 
-                                        <Route path='/dashboard' element={<DashBoard />} />
+                                        {/* <Route path='/dashboard' element={<DashBoard />} /> */}
                                         <Route path='/setting' element={<RolePage />} />
                                         <Route path='/user' element={<UserPage />} />
                                         <Route path='/category_supplies' element={<SuppliesCategoryPage />} />
                                         <Route path='/supplies' element={<SuppliesPage />} />
-                                        <Route path='/supplier' element={<SupplierPage />} />
-                                        <Route path='/shopping' element={<ShoppingPage />} />
-                                        <Route path='/shop' element={<NewPurchase />} />
+                                        {/* <Route path='/supplier' element={<SupplierPage />} /> */}
+                                        {/* <Route path='/shopping' element={<ShoppingPage />} /> */}
+                                        {/* <Route path='/shop' element={<NewPurchase />} /> */}
                                         <Route path='/category_product' element={<ProductCategoryPage />} />
                                         <Route path='/product' element={<ProductPage />} />
                                         <Route path='/waiter' element={<WaiterPage />} />
                                         <Route path='/alert' element={<Alert />} />
                                         {/* <Route path='/instructions' element={<Instruction />} /> */}
                                         <Route path='/create_product' element={<ProductDetails />} />
-                                        <Route path='/sale' element={<ViewSales />} />
-                                        <Route path='/sales' element={<Sales />} />
+                                        {/* <Route path='/sale' element={<ViewSales />} /> */}
+                                        {/* <Route path='/sales' element={<Sales />} /> */}
                                       </Route>
                                     </Routes>
                                   </ErrorBoundary>
-                                </Supplier>
-                              </ProductProvider>
-                            </ProductCategoriesProvider>
-                          </SaleProvider>
-                        </ShoppingProvider>
+                                {/* </Supplier> */}
+                              {/* </ProductProvider> */}
+                            {/* </ProductCategoriesProvider> */}
+                          {/* </SaleProvider> */}
+                        {/* </ShoppingProvider> */}
                       </Losses>
                     </Supplies>
                   </Product>
                 </CategoryProducts>
               </CategorySupplies>
-            </DashboardProvider>
+            {/* </DashboardProvider> */}
           </Module>
         </User>
       </Role>

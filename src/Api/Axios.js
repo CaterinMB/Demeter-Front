502da6486fc76224cd236337893309edf10f7ab1
CaterinMB <cmartinez2131@misena.edu.co>
2023-12-19 00:20:28 -0500

    url

--- src/Api/Axios.js
+++ src/Api/Axios.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://10.10.10.202:5080',
-    // baseURL: 'https://demeter-back-production.up.railway.app',
+    // baseURL: 'http://10.10.10.202:5080',
+    baseURL: 'http://44.242.125.70:5080',
     withCredentials: true
 })
 
